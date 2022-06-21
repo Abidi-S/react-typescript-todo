22d2fc580a73e20b4e710e8a1c848d957ac187b3
absara428 <absara428@gmail.com>
2022-06-21 15:50:11 +0100

    first commit, before reusable todo

---
+++ src/components/InputField.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./styles.css";
+
+interface Props {
+    toDo: string;
+    setToDo: React.Dispatch<React.SetStateAction<string>>
+
+}
+
+const InputField = ({ toDo, setToDo }: Props) => {
+  return <form className="input">
+    <input type="input"
+        value={toDo}
+        onChange={
+            (e)=>setToDo(e.target.value)
+        }
+        placeholder="Enter a task"
+        className="input_box">
+        {/* BEM convention of writing css class names */}
+    </input>
+    <button className="input_submit" type="submit">Go</button>
+  </form>
+}
+
+export default InputField
